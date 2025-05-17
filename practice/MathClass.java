b6c7642516daa5eed445d5cfeba57789bb79ceae
krishmamaharjan <krishmamaharjan73@gmail.com>
2025-05-17 21:19:05 +0545

    Math java

---
+++ practice/MathClass.java
@@ -0,0 +1,25 @@
+package practice;
+
+
+public class MathClass {
+        public static void main(String[] args) {
+            // Math class
+
+        System.out.println(Math.PI);
+        System.out.println(Math.E);
+
+        double result;
+
+        result = Math.pow(2,3);
+        result = Math.abs(-5);
+        result = Math.sqrt(9);
+        result = Math.round(3.14);
+        result = Math.ceil(3.14);
+        result = Math.floor(3.14);
+        result = Math.max(10,20);
+        result = Math.min(10,20);
+
+        System.out.println(result);
+
+}
+}
