1b4b6437e03aac299f380d508313b546514ef83e
krishmamaharjan <krishmamaharjan73@gmail.com>
2025-05-17 21:27:44 +0545

    Math class

--- practice/MathClass.java
+++ practice/MathClass.java
@@ -1,5 +1,5 @@
 package practice;
-
+import java.util.Scanner;
 
 public class MathClass {
         public static void main(String[] args) {
@@ -20,6 +20,28 @@ public class MathClass {
         result = Math.min(10,20);
 
         System.out.println(result);
+
+        // Hypotenuse of right angle c= sqrt(a2 + b2)
+
+        Scanner scanner = new Scanner(System.in);
+
+        double a;
+        double b;
+        double c;
+
+        System.out.print("Enter the length of side A : ");
+        a = scanner.nextDouble();
+
+        System.out.print("Enter the length of side B : ");
+        b = scanner.nextDouble();
+
+        c = Math.sqrt(Math.pow(a,2) + Math.pow(b,2));
+
+        System.out.println("The hypotenuse (side c) is " + c );
+
+        scanner.close();
+
+
 
 }
 }
